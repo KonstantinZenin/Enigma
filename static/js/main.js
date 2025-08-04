87c914b0dd51e32e9fcd477d013b1c97f824aa02
Konstantin <Kostick_zenin@mai.ru>
2025-08-04 20:51:28 +0300

    fix: правки и доработка базового шаблона

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Страница загружена полностью!');
-    alert('Страница загружена полностью!');
-});
-
 document.querySelectorAll('.nav-link').forEach(link => {
     if(link.href === window.location.href) {
         link.classList.add('active');
